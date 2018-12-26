1aa4353ce7b600443f070ec716717089892ab0f2
Jeff Bullock <thetanbull@gmail.com>
2018-12-26 15:39:18 -0500

    Create booWho.js
    freeCodeCamp Basic Algorithm

---
+++ freeCodeCamp/JavaScript/booWho.js
@@ -0,0 +1,15 @@
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+function booWho(bool) {
+    if(bool === true){
+        return true;
+    }
+    if(bool === false){
+        return true;
+    }
+    return false;
+}
+  
+booWho(null);
