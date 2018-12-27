898c66f1b0225aa75a437e572a5c0a838f2973db
Jeff Bullock <thetanbull@gmail.com>
2018-12-26 21:34:06 -0500

    Create spinalTapCase.js

---
+++ freeCodeCamp/JavaScript/spinalTapCase.js
@@ -0,0 +1,13 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+    var spinalStr = str.replace(/([A-Z])/g, " $1").trim();
+    //console.log(spinalStr);
+    spinalStr = spinalStr.toLowerCase().replace(/[_\s]+/g, "-");
+    console.log(spinalStr);
+    return spinalStr;
+  }
+  
+  spinalCase('This Is Spinal Tap');
