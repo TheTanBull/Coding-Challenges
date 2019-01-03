c75425219bcf562adf19f7dcd0a1a89f20fecccf
Jeff Bullock <thetanbull@gmail.com>
2019-01-02 21:11:33 -0500

    Create symmetricDifference.js

---
+++ freeCodeCamp/Coding Interview Algorithms/symmetricDifference.js
@@ -0,0 +1,41 @@
+// Code by Jeff "Tan" Bullock 
+// @TheTanBull
+
+function sym() {
+    let symDifArr = [], sets = [];
+    var argsArray = Array.prototype.slice.call(arguments, 0);
+    //console.log(argsArray);
+    argsArray.forEach(function(a){
+        //console.log(a);
+        sets.push(new Set(a));
+    });
+    //console.log(sets);
+    argsArray.forEach(function(arr, index){
+        //console.log(arr);
+        arr.forEach(function(e){
+            //console.log(e);
+            var duplicate = false;
+            for(var i = 0; i < sets.length; i++){
+                //console.log(i + " and the " + index);
+                if(i != index) {
+                    if (sets[i].has(e)){
+                        //console.log(sets[i]);
+                        //console.log(e);
+                        duplicate = true;
+                        break;
+                    }
+                }
+            }
+            if(!duplicate){
+                symDifArr.push(e);
+            }
+        });
+    });
+    symDifArr.sort(function(a, b){
+       return a - b; 
+    });
+    console.log(symDifArr);
+    return symDifArr;
+  }
+  
+  sym([1, 2, 3], [5, 2, 1, 4]);
