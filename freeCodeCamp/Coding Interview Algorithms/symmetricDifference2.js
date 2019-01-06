0e4bc682bda30b2c2686df66e98f9376476c0d99
Jeff Bullock <thetanbull@gmail.com>
2019-01-05 20:52:22 -0500

    Update symmetricDifference2.js
    Algorithm is functional and passes the test conditions.

--- freeCodeCamp/Coding Interview Algorithms/symmetricDifference2.js
+++ freeCodeCamp/Coding Interview Algorithms/symmetricDifference2.js
@@ -2,14 +2,14 @@
 // @TheTanBull
 
 function sym() {
-    var cleanArray = [], sets = [], symDifArr = []; // Array to handle duplicates
+    var cleanArray = [], sets = [], symDifArr = [], compareArray = []; // Array to handle duplicates
     var argsArray = Array.prototype.slice.call(arguments, 0);
     // This block of code will return each array with
     // no duplicates.
     argsArray.forEach(function(argsIndex){
         //console.log(argsIndex);
         var numExistsArr = [];
-        tempArray = argsIndex.filter(function(n){
+        let tempArray = argsIndex.filter(function(n){
             if(numExistsArr.indexOf(n) == -1){
                 numExistsArr.push(n);
                 return true;
@@ -18,40 +18,47 @@ function sym() {
         cleanArray.push(tempArray);
     });
 
-    cleanArray.forEach(function(a){
-        //console.log(a);
-        sets.push(new Set(a));
-    });
-    //console.log(sets);
-    cleanArray.forEach(function(arr, index){
-        //console.log(arr);
-        arr.forEach(function(e){
-            //console.log(e);
-            var duplicate = false;
-            for(var i = 0; i < sets.length; i++){
-                //console.log(i + " and the " + index);
-                if(i != index) {
-                    if (sets[i].has(e)){
-                        //console.log(sets[i]);
-                        //console.log(e);
-                        duplicate = true;
-                        break;
+    cleanArray.forEach(function(arrayIndex){
+        sets.push(new Set(arrayIndex));
+    })
+
+    while(cleanArray.length > 1){
+        compareArray.push(cleanArray[0]);
+        compareArray.push(cleanArray[1]);
+        let tempArray = [];
+        compareArray.forEach(function(arr, index){
+            arr.forEach(function(e){
+                var duplicate = false;
+                for(var i = 0; i < 2; i++){
+                    if(i != index){
+                        if(sets[i].has(e)){
+                            duplicate = true;
+                            break;
+                        }
                     }
                 }
-            }
-            if(!duplicate){
-                symDifArr.push(e);
-            }
+                if(!duplicate){
+                    tempArray.push(e);
+                }
+            });
         });
-    });
-    symDifArr.sort(function(a, b){
+        cleanArray.shift();
+        cleanArray.shift();
+        cleanArray.unshift(tempArray);
+        sets.shift();
+        sets.shift();
+        sets.unshift(new Set(tempArray));
+        compareArray = [];
+    }
+
+    cleanArray[0].sort(function(a, b){
        return a - b; 
     });
-    console.log(symDifArr);
-    return symDifArr;
+    console.log(cleanArray[0]);
+    return cleanArray[0];
   
 
     
   }
   
-  sym([1, 2, 3], [5, 5, 2, 1, 4]);
+  sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
