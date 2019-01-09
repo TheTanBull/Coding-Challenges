c1d6d4a5680f35dc7a2f635607be185f303f85ee
Jeff Bullock <thetanbull@gmail.com>
2019-01-09 12:52:19 -0500

    Update pairwise.js
    Algorithm currently adds all index matches

--- freeCodeCamp/Coding Interview Algorithms/pairwise.js
+++ freeCodeCamp/Coding Interview Algorithms/pairwise.js
@@ -1,2 +1,25 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
+
+function pairwise(arr, arg) {
+    var indexMatch = [];
+    for(var i = 0; i < arr.length; i++){
+        for(var j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === arg){
+                if(indexMatch.indexOf(arr[i] == -1)){
+                    indexMatch.push(i);
+                    indexMatch.push(j);
+                }
+                console.log("True");
+            }
+        }
+    }
+    console.log(indexMatch);
+    var indexSum = indexMatch.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+    console.log(indexSum);
+    return indexSum;
+  }
+  
+  pairwise([1,1,1], 2);
