8b117384c6800462fbb88ce368929f669159175e
Jeff Bullock <thetanbull@gmail.com>
2019-01-21 12:56:37 -0500

    Update twoSum.js
    Added a map solution to twoSum.

--- Leetcode/twoSum.js
+++ Leetcode/twoSum.js
@@ -1,7 +1,8 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
 
-var twoSum = function(nums, target) {
+//BruteForce for-loop
+var twoSumBF = function(nums, target) {
     for(var i = 0; i < nums.length - 1; i++){
         for(var j = i + 1; j < nums.length; j++){
             if(nums[i] + nums[j] == target){
@@ -9,6 +10,27 @@ var twoSum = function(nums, target) {
             }
         }
     }
+}
+
+/***************************************** */
+
+//Map Solution
+
+var twoSum = function(nums, target){
+    var valueMap = new Map();
+    var sums = [];
+
+    for(var i = 0; i < nums.length; i++){      
+        var targetDifference = target - nums[i];
+
+        if(valueMap.get(targetDifference) !== undefined){
+            sums.push(valueMap.get(targetDifference));
+            sums.push(i);
+            return sums;
+        }
+        valueMap.set(nums[i], i);
+    }
+
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
