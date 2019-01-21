aae28bb0e17250cb6be0680e0a150703097312a8
Jeff Bullock <thetanbull@gmail.com>
2019-01-21 11:49:42 -0500

    For loop solution
    Added a for-loop solution

--- Leetcode/twoSum.js
+++ Leetcode/twoSum.js
@@ -1,2 +1,14 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
+
+var twoSum = function(nums, target) {
+    for(var i = 0; i < nums.length - 1; i++){
+        for(var j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j];
+            }
+        }
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
