1f39df908196e0a248a29012dfc985f4ef6cf0c9
Jeff Bullock <thetanbull@gmail.com>
2019-01-24 20:27:52 -0500

    Update reverseStringE.js
    Test Passed.

--- Leetcode/reverseStringE.js
+++ Leetcode/reverseStringE.js
@@ -4,14 +4,13 @@
 // Write a function that reverses a string. The input string is given as an array of characters char[].
 
 var reverseString = function(s) {
-    strIndexLength = s.length - 1;
-    for(var i = 0; i < Math.floor(strIndexLength / 2); i++){
+    var strIndexLength = s.length - 1;
+    for(var i = 0; i <= Math.floor(strIndexLength / 2); i++){
         var temp = s[i];
         s[i] = s[strIndexLength - i];
         s[strIndexLength - i] = temp;
     }
     console.log(s);
-    return s;
 };
 
-reverseString(["h","e","l","l","o","n"]);
+reverseString(["H","E","L","P","O","A"]);
