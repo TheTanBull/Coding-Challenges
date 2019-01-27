d71ce07d46bb88199a968c8aec4ec55faec31256
Jeff Bullock <thetanbull@gmail.com>
2019-01-26 19:39:08 -0500

    Update binaryTreeDepthE.js
    Implemented recursive solution

--- binaryTreeDepthE.js
+++ binaryTreeDepthE.js
@@ -1,3 +1,15 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
 
+var maxDepth = function(root) {
+    let max = -1;
+    const getMaxLevel = (node, depth=0) => {
+      if (node) {
+        if (depth > max) max = depth;
+        getMaxLevel(node.left, depth + 1);
+        getMaxLevel(node.right, depth + 1);
+      }
+    }
+    getMaxLevel(root);
+    return max + 1;
+  };
