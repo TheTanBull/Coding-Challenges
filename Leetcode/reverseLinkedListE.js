dbb55913b1dfceed79e2f2c141b876f1e5a0a9e6
Jeff Bullock <thetanbull@gmail.com>
2019-01-29 22:22:48 -0500

    Update reverseLinkedListE.js
    Implemented recursive solution

--- Leetcode/reverseLinkedListE.js
+++ Leetcode/reverseLinkedListE.js
@@ -2,5 +2,31 @@
 // @TheTanBull
 
 var reverseList = function(head) {
-    
+    if(head === null || head.next === null){
+        console.log("We've hit the bottom");
+        return head;
+    }else{
+        var newHead = reverseList(head.next);
+        console.log("Current Node is " + head.val);
+        head.next.next = head;
+        head.next = null;
+    }
+    return newHead;
 };
+
+reverseList({
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: {
+                val: 4,
+                next: {
+                    val: 5,
+                    next: null
+                }
+            }
+        }
+    }
+})
