db09c78333caa07f9129fe506b01f5a67d27543b
Jeff Bullock <thetanbull@gmail.com>
2019-01-30 20:01:17 -0500

    Update deleteNode.js
    Space and time complexity O(1);

--- Leetcode/deleteNode.js
+++ Leetcode/deleteNode.js
@@ -1,6 +1,8 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
+// 80ms; faster than 100% of submissions
 
 var deleteNode = function(node) {
-    
+    node.val = node.next.val;
+    node.next = node.next.next;
 };
