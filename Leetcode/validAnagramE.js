01bb85d910d615e467184f10c75011ddefc92ae4
Jeff Bullock <thetanbull@gmail.com>
2019-02-14 09:36:42 -0500

    Update validAnagramE.js
    Implemented valid solution using maps.

--- Leetcode/validAnagramE.js
+++ Leetcode/validAnagramE.js
@@ -1,16 +1,12 @@
 // Code by Jeff "Tan" Bullock 
 // @TheTanBull
 //Given two strings s and t , write a function to determine if t is an anagram of s.
-
-function isEmptyObject(obj) {
-    for (var key in obj) {
-        return false;
-    }
-    return true;
-}
+//Runtime: 96 ms, faster than 48.71% of JavaScript online submissions for Valid Anagram.
+//Memory Usage: 36.4 MB, less than 100.00% of JavaScript online submissions for Valid Anagram.
 
 var isAnagram = function(s, t) {
     var myMap = new Map();
+    var isValidAnagram = true;
     for(var i = 0; i < s.length; i++){
         if(myMap.has(s[i])){
             myMap.set(s[i], myMap.get(s[i]) + 1);
@@ -30,12 +26,18 @@ var isAnagram = function(s, t) {
         }
     }
 
-    var isValidArray = isEmptyObject(myMap);
-    console.log(isValidArray);
+    myMap.forEach(function(v, k){
+        if(v === 0){
+            isValidAnagram = true;
+        } else{
+            isValidAnagram = false;
+        }
+    })
+    console.log(isValidAnagram);
     console.log(myMap);
-    return true;
+    return isValidAnagram;
 };
 
 
 
-isAnagram("ab", "ba");
+isAnagram("ab", "baf");
